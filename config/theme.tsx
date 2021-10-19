224bc15b03f5a78833f59de89057d5d225aca5fe
Creo-KR <gjwlsdn0877@naver.com>
2021-10-19 17:53:03 +0900

    Remove Tab indicator

--- config/theme.tsx
+++ config/theme.tsx
@@ -2,6 +2,24 @@ import { createTheme, Theme } from "@mui/material";
 import { grey, red } from "@mui/material/colors";
 
 export const defaultTheme: Theme = createTheme({
+  components: {
+    MuiTab: {
+      styleOverrides: {
+        root: {
+          "&.Mui-selected": {
+            backgroundColor: grey.A400,
+          },
+        },
+      },
+    },
+    MuiTabs: {
+      styleOverrides: {
+        indicator: {
+          display: "none",
+        },
+      },
+    },
+  },
   palette: {
     primary: {
       main: grey.A700,
